908378e0a95fda65b19fc9aeb6e2eadbfc5571cc
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-05-20 17:51:08 +0200

    add class card buy shop

--- resources/views/comic_view.blade.php
+++ resources/views/comic_view.blade.php
@@ -11,13 +11,61 @@
                     <div class="col-2 ">
                         <div class="ms_card">
                             <div class="ms_image mb-2">
-                                <img src="{{$singlecomic['thumb']}}" alt="">
+                                <img src="{{ $singlecomic['thumb'] }}" alt="">
                             </div>
-                            <div class="text-white fs-6">{{$singlecomic['title']}}</div>
+                            <div class="text-white ms_title">{{ $singlecomic['title'] }}</div>
                         </div>
                     </div>
                 @endforeach
             </div>
+            <div class="d-flex justify-content-center">
+                <button class="text-white ms_btn">LOAD MORE</button>
+            </div>
         </div>
+    </section>
+    <section class="ms_icon_section">
+        <div class="ms_container">
+            <div class="row justify-content-between text-white">
+                <div class="col d-flex align-items-center gap-2 mt-5 mb-5">
+                    <div class="ms_icon_image">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    </div>
+                    <div class="ms_title">
+                        DIGITAL COMICS
+                    </div>
+                </div>
+                <div class="col d-flex align-items-center gap-2 mt-5 mb-5">
+                    <div class="ms_icon_image">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    </div>
+                    <div class="ms_title">
+                        DC MERCHANDISE
+                    </div>
+                </div>
+                <div class="col d-flex align-items-center gap-2 mt-5 mb-5">
+                    <div class="ms_icon_image">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    </div>
+                    <div class="ms_title">
+                        SUBSCRIPTION
+                    </div>
+                </div>
+                <div class="col d-flex align-items-center gap-2 mt-5 mb-5">
+                    <div class="ms_icon_image">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    </div>
+                    <div class="ms_title">
+                       COMIC SHOP LOCATOR
+                    </div>
+                </div>
+                <div class="col d-flex align-items-center gap-2 mt-5 mb-5">
+                    <div class="ms_icon_image">
+                        <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    </div>
+                    <div class="ms_title">
+                        DIGITAL COMICS
+                    </div>
+                </div>
+            </div>
     </section>
 @endsection
