b7ba78ed4f1e5a111f52aea7173940cc6cb4a747
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-05-20 17:58:15 +0200

    add file footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="footer_top">
+        <div class="ms_container">
+
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="ms_container">
+
+        </div>
+    </div>
+</footer>
